774f13bcf505445fabec34f5bce59f68eb1a29fe
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-19 18:29:01 -0700

    revised landing page language

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,11 +9,12 @@ export default function Landing() {
             on the internet.
             <br /><br />
             Right now, this place is currently under construction. If you arrived here looking to
-            contact me, please send an e-mail to <b>info@wildcharger.xyz</b>. Otherwise, use the
-            navigation bar above to check out one of my other projects. Once complete, the website
-            will feature a more comprehensive expression of who I am & what I&apos;ve done over the
-            years, so if you&apos;re interested, feel free to check back every so often in the 
-            event I am able to spruce this place up a bit better.
+            contact me, please send an e-mail to <b>info@wildcharger.xyz</b>, or use the contact
+            form on the Contact page.
+            <br /><br />
+            Once complete, the website will feature a more comprehensive expression of who I am & 
+            what I&apos;ve done over the years, so if you&apos;re interested, feel free to check 
+            back every so often in the event I am able to spruce this place up a bit better.
             <br /><br />
             Until then, thanks for visiting!
         </p>
