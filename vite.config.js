77b2cfdff64610e06a0f6e3db4c377f1267e1a73
WildChargerTV <46546991+WildChargerTV@users.noreply.github.com>
2025-01-24 18:08:10 -0800

    Removed references to a nonexistent backend

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,15 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    eslint({
-      lintOnStart: true,
-      failOnError: mode === 'production'
-    })
-  ],
-  rules: {
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
-  },
-  server: {
-    proxy: { 
-        '/api': 'http://localhost:8000',
-        '/img-assets': 'http://localhost:8000'
+    plugins: [
+        react(),
+        eslint({
+        lintOnStart: true,
+        failOnError: mode === 'production'
+        })
+    ],
+    rules: {
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
     }
-  }
 }));
